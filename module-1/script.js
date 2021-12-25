2ffb8fa47f1ea6cb5f41bfe1ee5d4cb33e99cc7a
Pablo Montenegro <montenegro.pablo@outlook.com>
2021-12-25 15:08:15 +0100

    Add code for module 1 - class 7

--- module-1/script.js
+++ module-1/script.js
@@ -4,9 +4,21 @@ var context = canvas.getContext("2d");
 context.fillStyle = "#FF0000";
 
 var heroPosition = { x: 50, y: 50 };
+var speed = 3;
 context.fillRect(
   heroPosition.x,
   heroPosition.y,
+  5,
+  5
+);
+
+var itemPosition = {
+  x: Math.floor(Math.random() * canvas.width),
+  y: Math.floor(Math.random() * canvas.height),
+};
+context.fillRect(
+  itemPosition.x,
+  itemPosition.y,
   5,
   5
 );
@@ -23,22 +35,22 @@ window.onkeydown = function(event) {
 
   switch (event.key) {
     case 'ArrowUp': {
-      heroPosition.y = heroPosition.y - 1;
+      heroPosition.y = heroPosition.y - speed;
       break;
     }
 
     case 'ArrowDown': {
-      heroPosition.y = heroPosition.y + 1;
+      heroPosition.y = heroPosition.y + speed;
       break;
     }
 
     case 'ArrowLeft': {
-      heroPosition.x = heroPosition.x - 1;
+      heroPosition.x = heroPosition.x - speed;
       break;
     }
 
     case 'ArrowRight': {
-      heroPosition.x = heroPosition.x + 1;
+      heroPosition.x = heroPosition.x + speed;
       break;
     }
   }
@@ -49,4 +61,46 @@ window.onkeydown = function(event) {
     5,
     5
   );
+
+  context.fillRect(
+    itemPosition.x,
+    itemPosition.y,
+    5,
+    5
+  );
+
+  if (
+    heroPosition.x < itemPosition.x + 5
+    && heroPosition.x > itemPosition.x - 5
+    && heroPosition.y < itemPosition.y + 5
+    && heroPosition.y > itemPosition.y - 5
+  ) {
+//    console.log("touch");
+    context.clearRect(
+      0,
+      0,
+      canvas.width,
+      canvas.height
+    );
+
+    context.fillRect(
+      heroPosition.x,
+      heroPosition.y,
+      5,
+      5
+    );
+
+    itemPosition = {
+      x: Math.floor(Math.random() * canvas.width),
+      y: Math.floor(Math.random() * canvas.height),
+    };
+
+    context.fillRect(
+      itemPosition.x,
+      itemPosition.y,
+      5,
+      5
+    );
+  }
 }
+
