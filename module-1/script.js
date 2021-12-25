62f888f3c5ab59722c8a41d59d50bfa6ebed6de5
Pablo Montenegro <montenegro.pablo@outlook.com>
2021-12-25 14:52:09 +0100

    Add code for module 1 - class 6

--- module-1/script.js
+++ module-1/script.js
@@ -2,19 +2,51 @@ var canvas = document.getElementById("myCanvas");
 // console.log(canvas);
 var context = canvas.getContext("2d");
 context.fillStyle = "#FF0000";
-// context.fillRect(0, 0, 50, 40);
 
-canvas.onpointerdown = function(event) {
-  canvas.onpointermove = function(event) {
-    context.fillRect(
-      event.layerX,
-      event.layerY,
-      5,
-      5
-    );
+var heroPosition = { x: 50, y: 50 };
+context.fillRect(
+  heroPosition.x,
+  heroPosition.y,
+  5,
+  5
+);
+
+
+window.onkeydown = function(event) {
+//  console.log(event);
+  context.clearRect(
+    0,
+    0,
+    canvas.width,
+    canvas.height
+  );
+
+  switch (event.key) {
+    case 'ArrowUp': {
+      heroPosition.y = heroPosition.y - 1;
+      break;
+    }
+
+    case 'ArrowDown': {
+      heroPosition.y = heroPosition.y + 1;
+      break;
+    }
+
+    case 'ArrowLeft': {
+      heroPosition.x = heroPosition.x - 1;
+      break;
+    }
+
+    case 'ArrowRight': {
+      heroPosition.x = heroPosition.x + 1;
+      break;
+    }
   }
-}
 
-canvas.onpointerup = function(event) {
-  canvas.onpointermove = null;
+  context.fillRect(
+    heroPosition.x,
+    heroPosition.y,
+    5,
+    5
+  );
 }
