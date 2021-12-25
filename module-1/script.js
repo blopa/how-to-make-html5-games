04c5f6b221044588c516fe44d2cecb60b35a58ca
Pablo Montenegro <montenegro.pablo@outlook.com>
2021-12-25 14:26:17 +0100

    Add code for module 1 - class 4

--- module-1/script.js
+++ module-1/script.js
@@ -2,4 +2,26 @@ var canvas = document.getElementById("myCanvas");
 // console.log(canvas);
 var context = canvas.getContext("2d");
 context.fillStyle = "#FF0000";
-context.fillRect(0, 0, 50, 40);
+// context.fillRect(0, 0, 50, 40);
+
+canvas.onclick = function(event) {
+  console.log("clicky");
+}
+
+canvas.onpointerdown = function(event) {
+    context.fillRect(
+        event.layerX,
+        event.layerY,
+        5,
+        5
+    );
+}
+
+canvas.onpointerup = function(event) {
+    context.fillRect(
+        event.layerX,
+        event.layerY,
+        5,
+        5
+    );
+}
