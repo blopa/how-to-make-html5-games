137f51ce16f764d52a54924da8f41f140884cfe7
Pablo Montenegro <montenegro.pablo@outlook.com>
2021-12-25 14:15:08 +0100

    Add code for module 1 - class 3

--- module-1/script.js
+++ module-1/script.js
@@ -1,1 +1,5 @@
-console.log("hello world 2");
+var canvas = document.getElementById("myCanvas");
+// console.log(canvas);
+var context = canvas.getContext("2d");
+context.fillStyle = "#FF0000";
+context.fillRect(0, 0, 50, 40);
