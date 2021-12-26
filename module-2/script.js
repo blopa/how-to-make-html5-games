7d9e257cdd9155c1e0f34b881f35e52f4b0e6565
Pablo Montenegro <montenegro.pablo@outlook.com>
2021-12-26 21:00:25 +0100

    Add code for module 2 - class 3

--- module-2/script.js
+++ module-2/script.js
@@ -22,6 +22,16 @@ window.onkeyup = function(event) {
   actionKeyPressed = false;
 }
 
+// pipes
+var pipeWidth = 20
+var holeSize = 80;
+var initialHoles = [
+  { x: 100, y: 50 },
+  { x: 200, y: 50 },
+  { x: 300, y: 50 },
+  { x: 400, y: 50 },
+];
+
 // this runs 60 times per second
 window.setInterval(function() {
   context.clearRect(
@@ -56,6 +66,30 @@ window.setInterval(function() {
   ) {
     acceleration = 0;
   }
+
+  initialHoles.forEach(function (hole) {
+    hole.x = hole.x - 1;
+
+    if (hole.x + pipeWidth <= 0) {
+      hole.x = canvas.width;
+    }
+
+    // fill a full height line
+    context.fillRect(
+      hole.x,
+      0,
+      pipeWidth,
+      canvas.height
+    );
+
+    // clear rect inside the pipe lines
+    context.clearRect(
+      hole.x,
+      hole.y,
+      holeSize,
+      holeSize
+    );
+  });
 
   context.fillRect(
     heroPosition.x,
