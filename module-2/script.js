9b96f8a899a8e7435898fdc6267a70fb491805d6
Pablo Montenegro <montenegro.pablo@outlook.com>
2021-12-26 20:57:13 +0100

    Add code for module 2 - class 2

--- module-2/script.js
+++ module-2/script.js
@@ -8,6 +8,19 @@ var heroPosition = { x: 50, y: 50 };
 
 // physics
 var gravity = 2;
+var acceleration = 0;
+var maxAcceleration = 12;
+
+// controls
+var actionKey = "Space";
+var actionKeyPressed = false;
+window.onkeydown = function(event) {
+  actionKeyPressed = event.code === actionKey;
+}
+
+window.onkeyup = function(event) {
+  actionKeyPressed = false;
+}
 
 // this runs 60 times per second
 window.setInterval(function() {
@@ -18,10 +31,31 @@ window.setInterval(function() {
     canvas.height
   );
 
+
+
   heroPosition.y = Math.min(
     canvas.height - heroSize,
-    heroPosition.y + gravity
+    heroPosition.y + (gravity - acceleration)
   );
+
+  if (actionKeyPressed) {
+    actionKeyPressed = false;
+    acceleration = gravity;
+  }
+
+  if (acceleration) {
+    acceleration = Math.min(
+      acceleration + 1,
+      maxAcceleration
+    );
+  }
+
+  if (
+    acceleration === maxAcceleration
+    || heroPosition.y < 0
+  ) {
+    acceleration = 0;
+  }
 
   context.fillRect(
     heroPosition.x,
